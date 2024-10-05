6588f7a28c2237e3edb6e78eb2bd807c5336c67c
Samuel <samueldeweerd@gmail.com>
2024-10-05 11:34:30 +0200

    Updating requests in available places component so that place ID is sent to backend upon click.

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -14,18 +14,36 @@ import { map } from 'rxjs';
   imports: [PlacesComponent, PlacesContainerComponent],
 })
 export class AvailablePlacesComponent implements OnInit {
+  isFetching = signal(false);
+  error = signal('');
+  
   places = signal<Place[] | undefined>(undefined);
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
+    this.isFetching.set(true);
     const subscription = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places')
-    .pipe(map((restData) => restData.places))
+    .pipe(map((restData) => restData.places), catchError((error) => throwError(() => new Error('Something went wrong fetching the available places'))))
     .subscribe(
-      { next: (places) => this.places.set(places) }
+      { next: (places) => this.places.set(places),
+        complete: () => this.isFetching.set(false),
+        error: (error: Error) => {
+          console.log(error);
+          this.error.set(error.message)
+        }
+       }
     );
 
     this.destroyRef.onDestroy(() => subscription.unsubscribe);
+
+  }
+
+
+  onSelectPlace(selectedPlace: Place) {
+    this.httpClient.put('http://localhost:3000/user-places', {placeId: selectedPlace.id}).subscribe(
+      {next: (resData) => console.log(resData)}
+    );
 
   }
 }
